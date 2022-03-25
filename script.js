61539f073a79b63aa223d03c71c0bf1fb0cb7875
blowflow-code <b.flow888@gmail.com>
2022-03-25 15:24:03 +0100

    24/03 design editted
    edit overall site design
    디자인 함.

--- script.js
+++ script.js
@@ -5,27 +5,57 @@ let column_2 = document.getElementsByClassName("column2");
 let column_3 = document.getElementsByClassName("column3");
 let column_4 = document.getElementsByClassName("column4");
 let column_5 = document.getElementsByClassName("column5");
+let bookCover = document.getElementsByClassName("bookcover")
 
-function descriptionClick (event) {
+// function descriptionClick (event) {
 
-    for (let i = 0; i < column_2.length; i++) {
+//     for (let i = 0; i < column_2.length; i++) {
 
-        column_2[i].mousedown = function (event) {
+//         column_2[i].mousedown = function (event) {
     
-            let descriptions = description[i]
-            let column3 = column_3[i]
+//             let descriptions = description[i]
+//             let column3 = column_3[i]
     
-            if (descriptions.clientHeight > 0) {
-                description[i].style.height = "0px";
-                column3[i].style.transform = "translateY(0px)";
-            } else {
-                description[i].style.height = "400px";
-                column3[i].style.transform = "translateY(500px)";
+//             if (descriptions.clientHeight > 0) {
+//                 description[i].style.height = "0px";
+//                 column3[i].style.transform = "translateY(0px)";
+//             } else {
+//                 description[i].style.height = "400px";
+//                 column3[i].style.transform = "translateY(500px)";
     
-            }
-        }
-    }
+//             }
+//         }
+//     }
+// }
+// descriptionClick();
+
+
+for (let i = 0; i < column_2.length; i++) {
+
+  column_2[i].mousedown = function (event) {
+
+      let descriptions = description[i]
+      let column3 = column_3[i]
+
+      if (descriptions.clientHeight > 0) {
+          description[i].style.height = "0px";
+          // column3[i].style.transform = "translateY(0px)";
+      } else {
+          description[i].style.height = "400px";
+          // column3[i].style.transform = "translateY(500px)";
+
+      }
+  }
 }
+
+for (let i = 0; i < bookCover.length; i++) {
+
+  bookCover[i].mousedown = function (event) {
+    // let descriptions = description[i]
+    bookCover.style.width= "10%";
+  }
+}
+
 // /하고 싶은것 : column2를 눌렀을때 description 높이가 400으로 커지고 동시에 column3가 500px 아래로 내려감, 다시 누르면 원상태 복귀/ 
 // for (let i = 0; i < column_2.length; i++) {
 
@@ -62,3 +92,50 @@ function descriptionClick (event) {
 //     }
 // }
 
+
+
+// Contact-card
+const frame = document.getElementById('frame')
+const card = document.getElementById('card')
+const light = document.getElementById('light')
+
+let { x, y, width, height } = frame.getBoundingClientRect()
+
+function mouseMove(e) {
+  const left = e.clientX - x
+  const top = e.clientY - y
+  const centerX = left - width / 2
+  const centerY = top - height / 2
+  const d = Math.sqrt(centerX**2 + centerY**2)
+
+  card.style.boxShadow = `
+    ${-centerX / 5}px ${-centerY / 10}px 10px rgba(0, 0, 0, 0.2)
+  `
+
+  card.style.transform = `
+    rotate3d(${-centerY / 100}, ${centerX / 100}, 0, ${d / 8}deg)
+  `
+
+  light.style.backgroundImage = `
+    radial-gradient(circle at ${left}px ${top}px, #00000001, #ffffff11, #ffffff22)
+    `
+}
+
+frame.addEventListener('mouseenter', () => {
+  frame.addEventListener('mousemove', mouseMove)
+})
+
+frame.addEventListener('mouseleave', () => {
+  frame.removeEventListener('mousemove', mouseMove)
+  card.style.boxShadow = ''
+  card.style.transform = ''
+  light.style.backgroundImage = ''
+})
+
+window.addEventListener('resize', () => {
+  rect = frame.getBoundingClientRect()
+  x = rect.x
+  y = rect.y
+  width = rect.width
+  height = rect.height
+})
